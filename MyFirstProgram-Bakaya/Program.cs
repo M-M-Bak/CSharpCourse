28192daedf81cd00afddf749714f93bfc7c35068
Mazen <Mazen@DESKTOP-6DBSQLG>
2019-08-12 23:02:50 +0200

    Projektdateien hinzufügen.

---
+++ MyFirstProgram-Bakaya/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyFirstProgram_Bakaya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Bakaya!");
+            Console.ReadKey();
+        }
+    }
+}
